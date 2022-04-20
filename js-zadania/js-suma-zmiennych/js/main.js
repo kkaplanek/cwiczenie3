5c6db671fc9885231555dc721140337bb06ce435
kkaplanek <karolina.kaplanek@gmail.com>
2022-04-20 19:49:30 +0200

    js suma zmiennych

---
+++ js-zadania/js-suma-zmiennych/js/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 10;
+
+let secondNumber = 15;
+
+function sum (num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+
+console.log( sum (firstNumber, secondNumber) );
