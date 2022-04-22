3cbf94d51b3dfe9460b6e2d4464650586fe3edd0
kkaplanek <karolina.kaplanek@gmail.com>
2022-04-22 19:48:14 +0200

    js sprawdzenie liczby

---
+++ js-zadania/js-sprawdzenie-liczby/js/main.js
@@ -0,0 +1,10 @@
+
+function range (n) {
+    if (n >= 100 & n <= 200) {
+        console.log('Liczba znajduje się w przedziale');
+    } else if (n < 100 | n > 200) {
+        console.log('Liczba nie znajduje się w przedziale');
+    }
+}
+
+range(115);
