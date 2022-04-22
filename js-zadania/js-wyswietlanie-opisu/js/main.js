9784df3e3aaab59f22440931e0a06d2aeb675d69
kkaplanek <karolina.kaplanek@gmail.com>
2022-04-22 17:12:48 +0200

    js wyswietlanie opisu

---
+++ js-zadania/js-wyswietlanie-opisu/js/main.js
@@ -0,0 +1,7 @@
+let p1 = document.getElementById("opis");
+
+let przycisk = document.querySelector('button');
+
+przycisk.onclick = function () {
+    p1.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+}
