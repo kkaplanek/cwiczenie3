95c1d360696f15448c8d426a6f37f4578fca9d74
kkaplanek <karolina.kaplanek@gmail.com>
2022-04-23 11:53:57 +0200

    js sprawdzanie liczby2

---
+++ js-zadania/js-sprawdzanie-liczby2/js/main.js
@@ -0,0 +1,11 @@
+let one = true;
+
+function dwadziescia (num1, num2) {
+    if (num1 == 20) {
+    console.log(one);
+    } else if (num2 == 20) {
+        console.log(one);
+    } else if (num1 + num2 <= 20) {
+        console.log(one);
+    }
+}
