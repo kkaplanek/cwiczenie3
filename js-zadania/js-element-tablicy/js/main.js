49000f0520e44ae504a2d6ce22d708e48d254477
kkaplanek <karolina.kaplanek@gmail.com>
2022-04-25 14:46:52 +0200

    js element tablicy poprawka

--- js-zadania/js-element-tablicy/js/main.js
+++ js-zadania/js-element-tablicy/js/main.js
@@ -2,7 +2,7 @@ let array = [4, 8, 12, 40, 100];
 
 function first (tab1) {
     let pierwszy = tab1[0];
-    console.log(pierwszy);
+    return pierwszy;
 }
 
-first(array);
+console.log(first(array));
