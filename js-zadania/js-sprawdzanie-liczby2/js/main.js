b7258f78d9ca31851965a7459a93e9769bce111a
kkaplanek <karolina.kaplanek@gmail.com>
2022-04-25 14:30:56 +0200

    js sprawdzanie liczby 2 poprawka

--- js-zadania/js-sprawdzanie-liczby2/js/main.js
+++ js-zadania/js-sprawdzanie-liczby2/js/main.js
@@ -1,11 +1,11 @@
-let one = true;
-
 function dwadziescia (num1, num2) {
     if (num1 == 20) {
-    console.log(one);
+    return true;
     } else if (num2 == 20) {
-        console.log(one);
+        return true;
     } else if (num1 + num2 <= 20) {
-        console.log(one);
+        return true;
     }
 }
+
+console.log( dwadziescia(8, 12) );
